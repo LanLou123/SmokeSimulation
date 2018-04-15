3942f097a6305eaa30a6ac201a20cb18cfc340da
Lan lou <31492651+LanLou123@users.noreply.github.com>
2018-04-14 22:54:16 -0400

    !!

--- src/constants.cpp
+++ src/constants.cpp
@@ -18,4 +18,4 @@ const double theBuoyancyAlpha = 0.08; // Gravity's effect on the smoke particles
 const double theBuoyancyBeta = 0.37; // Buoyancy's effect due to temperature difference.	
 const double theBuoyancyAmbientTemperature = 0.0; // Ambient temperature.
 
-const double theVorticityEpsilon = 8;
+const double theVorticityEpsilon = 2;
